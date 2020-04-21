1317d358818dfbfa376d14638b0aaefeb687f44a
Julian Blagis <julianbgs96@gmail.com>
2020-04-21 17:04:13 +0200

    finish hover

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,1 +1,14 @@
 import './header.sass';
+
+export default () => {
+  let link = document.querySelector('.header__link_hover');
+  let tooltip = document.querySelector('.header__tooltip');
+
+  link.addEventListener('mouseover', () => {
+    tooltip.style.opacity = '1';
+  });
+
+  link.addEventListener('mouseout', () => {
+    tooltip.style.opacity = '0';
+  });
+}
